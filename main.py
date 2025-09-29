12be5fb30f2925e9c256dbbd71c630824edc6d67
darwinesc <darkin_drummer@hotmail.com>
2025-09-29 00:25:05 -0300

    Add - Read CSV file,remove duplicate and any null rows

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-    
+import pandas as pd    
+
 def main():
 
     try:
-       print("Home task")
+       
+        # Read csv file and create dataframe
+        df = pd.read_csv('input/test.csv')
+
+        total_rows = len(df.index)
+        print("Total rows: ", total_rows)
+
+        # Remove empty rows
+        df_empty_rows = df[(df.isnull() | (df == '')).all(axis=1)]
+        total_empty_rows = len(df_empty_rows)
+        print("Total empty rows: ", total_empty_rows)
+
+        # Remove duplicate rows
+        df_duplicates = df[df.duplicated()]
+        total_duplicate_rows = len(df_duplicates)
+        print("Total duplicates rows: ", total_duplicate_rows)
+
+        # Create a dataframe with discarted rows
+        df_discarted_rows = pd.concat([df_empty_rows, df_duplicates], ignore_index=True)
+
+        # Save the dataframe to csv file
+        df_discarted_rows.to_csv('output/discarded_rows.csv', index=False)
     
     except Exception as e:
         print(f"Error: {e}")
